3c2cc9e896eda8d4dc7555995952143c1311fd2a
frank <876700669@qq.com>
2024-04-27 17:38:23 +0800

    init:初始化仓库

---
+++ index.js
@@ -0,0 +1,7 @@
+let str = 'test';
+let num = 22;
+console.log(str);
+console.log(num);
+let v = null;
+str = `dddd${num}`;
+console.log(str);
---
+++ index.ts
@@ -0,0 +1,9 @@
+let str :string = 'test'
+let num:number = 22
+console.log(str)
+console.log(num)
+
+let v:void = null
+
+str = `dddd${num}`
+console.log(str)
