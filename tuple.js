95263720b3435a2bbb3088ef975351fdeb4f5759
frank <876700669@qq.com>
2024-05-28 11:10:40 +0800

    add:TS的元组类型

---
+++ tuple.js
@@ -0,0 +1,5 @@
+let tupleArr = [1, "hello"];
+// 元素的定义，只读的数组
+let tuple = [1, true, "hello", undefined];
+// 元素同样支持可选类型
+let tuple1 = [1];
---
+++ tuple.ts
@@ -0,0 +1,5 @@
+let tupleArr:[number,string] = [1,"hello"]
+// 元素的定义，只读的数组
+let tuple : readonly [number,boolean,string,undefined] = [1,true,"hello",undefined]
+// 元素同样支持可选类型
+let tuple1 :[x:number,y?:boolean] = [1]
